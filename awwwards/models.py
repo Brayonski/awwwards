d728fb953dad2b6c15a5b0fd45e4ae0d91ec6caf
root <root@localhost.localdomain>
2018-10-13 13:03:27 +0300

    projects & profile models

--- awwwards/models.py
+++ awwwards/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Projects(models.Model):
+    title = models.CharField(max_length=30)
+    image = models.ImageField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+class Profile(models.Model):
+    profile_pic = models.ImageField()
+    bio = models.TextField()
+    contact = models.TextField()
+
+    def __str__(self):
+        return self.text
